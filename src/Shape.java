617752eec3a55b2cc4634d221d34dcc4afa68204
osmankalkan <okalkan@posta.mu.edu.tr>
2025-04-25 11:57:05 +0300

    Add files via upload

--- src/Shape.java
+++ src/Shape.java
@@ -1,6 +1,7 @@
-public class Shape {
+public abstract class Shape {
 
-    private String color = "white";
+
+    private String color ="white";
 
     public Shape(){
 
@@ -8,23 +9,8 @@ public class Shape {
     public Shape(String color) {
         this.color = color;
     }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public double area(){
-        return 0;
-    }
-    public double perimeter(){
-        return 0;
-    }
-
-
+    public abstract double perimeter();
+    public abstract double area();
     @Override
     public String toString() {
         return "Shape{" +
